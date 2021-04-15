75bbe67e463a7cb78a556cad6a7b93c5a403eb33
unknown <allan.r.lima2017@gmail.com>
2021-04-14 21:30:52 -0300

    Projeto Módulo 1 Resilia

---
+++ GERALTFASE2.js
@@ -0,0 +1,17 @@
+setTimeout(function(){ {function Guerreiro(){
+    
+    var respondapergunta = prompt("Digite as opções correspondentes, mas lembre-se, sua jornada não será fácil");
+    if(!(respondapergunta=="1"||respondapergunta=="2")){
+    alert("Escolha 1 ou 2, sua vida depende disso!");
+    }
+    else if(respondapergunta=="1"){ 
+    location.assign('GERALTFASE2OPÇÃO1.html');
+    }
+    else if(respondapergunta=="2"){
+    alert("Parabéns, você conseguiu.");
+    location.assign('GERALTFASE3.html');
+    return
+    }}
+   
+}
+Guerreiro(); },900)
---
+++ HAROLDFASE3.js
@@ -0,0 +1,16 @@
+setTimeout(function(){ {function Guerreiro(){
+    
+    var respondapergunta = prompt("Digite as opções correspondentes, mas lembre-se, sua jornada não será fácil");
+    if(!(respondapergunta=="1"||respondapergunta=="2")){
+    alert("Escolha 1 ou 2, sua vida depende disso!");
+    }
+    else if(respondapergunta=="1"){ 
+    location.assign('HAROLDFASE3OPÇÃO1.html');
+    }
+    else if(respondapergunta=="2"){
+    location.assign('HAROLDFASE3OPÇÃO2.html');
+    return
+    }}
+   
+}
+Guerreiro(); },900)
